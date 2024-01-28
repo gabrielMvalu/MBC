112e696ea63f9ad8ae98c8bd07c2bb1073a584da
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 21:40:34 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pages.Plan_afaceri
+
+# Sidebar pentru navigație
+st.sidebar.title("Navigație")
+page = st.sidebar.selectbox("Alegeți o pagină:", ["Plan de Afaceri"])
+
+# Încărcarea paginilor
+if page == "Plan de Afaceri":
+    pages.Plan_afaceri.show_page()
