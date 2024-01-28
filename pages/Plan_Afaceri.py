130c33ef7932153c32979f69971418b4f5c7a252
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 22:40:40 +0200

    Update Plan_Afaceri.py

--- pages/Plan_Afaceri.py
+++ pages/Plan_Afaceri.py
@@ -3,17 +3,21 @@ import streamlit as st
 from docx import Document
 
 def load_docx(file_path):
-    # Încarcă documentul .docx
     doc = Document(file_path)
     full_text = []
-    for para in doc.paragraphs:  # Iterează prin fiecare paragraf
-        full_text.append(para.text)  # Adaugă textul paragrafului la lista de text
-    return '\n'.join(full_text)  # Întoarce textul complet ca un singur string
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n\n'.join(full_text)
 
 # Calea către fișierul .docx în folderul assets
 file_path = './assets/machetaPlan.docx'
-
-# Afișează conținutul .docx în aplicația Streamlit
 docx_text = load_docx(file_path)
-st.text(docx_text)
 
+# Afișează textul într-o zonă editabilă și permite consultantului să facă modificări
+editable_text = st.text_area("Editați textul după necesități:", value=docx_text, height=300)
+
+# Buton pentru salvarea modificărilor
+if st.button('Salvează Modificările'):
+    # Aici poți adăuga logica pentru salvarea textului modificat
+    # De exemplu, salvarea într-un nou document .docx sau într-o bază de date
+    st.success("Modificările au fost salvate cu succes!")
