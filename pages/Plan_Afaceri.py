8ce3f3435b8d4e14bb59acb81a5dbb33c853d417
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 22:35:15 +0200

    Update Plan_Afaceri.py

--- pages/Plan_Afaceri.py
+++ pages/Plan_Afaceri.py
@@ -1,5 +1,19 @@
 # pages/Plan_Afaceri.py
 import streamlit as st
+from docx import Document
 
-st.title("Plan de Afaceri")
-st.write("Aici veți putea încărca și edita planul de afaceri.")
+def load_docx(file_path):
+    # Încarcă documentul .docx
+    doc = Document(file_path)
+    full_text = []
+    for para in doc.paragraphs:  # Iterează prin fiecare paragraf
+        full_text.append(para.text)  # Adaugă textul paragrafului la lista de text
+    return '\n'.join(full_text)  # Întoarce textul complet ca un singur string
+
+# Calea către fișierul .docx în folderul assets
+file_path = './assets/machetaPlan.docx'
+
+# Afișează conținutul .docx în aplicația Streamlit
+docx_text = load_docx(file_path)
+st.text(docx_text)
+
