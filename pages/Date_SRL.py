97cae391880711014a81273d5c5a5bee29236bea
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 23:02:13 +0200

    Create Date_SRL.py

---
+++ pages/Date_SRL.py
@@ -0,0 +1,8 @@
+import streamlit as st
+ 
+# Creează un widget de încărcare a fișierului
+uploaded_file = st.file_uploader("Trageți fișierul aici sau faceți clic pentru a încărca un document", type=["docx"])
+
+if uploaded_file is not None:
+  st.success("Fișier încărcat cu succes!")
+  # Aici va fi adăugată logica de extracție a datelor din document
