e60f8303bb8021fa05a589279380c9d9fb8891c6
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 22:06:09 +0200

    Update Plan_Afaceri.py

--- pages/Plan_Afaceri.py
+++ pages/Plan_Afaceri.py
@@ -1,7 +1,5 @@
-# Plan_Afaceri.py
-
+# pages/Plan_Afaceri.py
 import streamlit as st
 
-def show_page():
-    st.title("Plan de Afaceri")
-    # Restul codului pentru pagina Plan de Afaceri
+st.title("Plan de Afaceri")
+st.write("Aici veți putea încărca și edita planul de afaceri.")
