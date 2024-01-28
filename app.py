289db3f29ab3bb2acd6f89ac4211eddc3444f329
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-28 22:05:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
+# app.py
 import streamlit as st
-import pages.Plan_Afaceri
 
-# Sidebar pentru navigație
-st.sidebar.title("Navigație")
-page = st.sidebar.selectbox("Alegeți o pagină:", ["Plan de Afaceri"])
-
-# Încărcarea paginilor
-if page == "Plan de Afaceri":
-    pages.Plan_Afaceri.show_page()
+st.title("Pagina Principală")
+st.write("Bine ați venit la aplicația multipagină!")
