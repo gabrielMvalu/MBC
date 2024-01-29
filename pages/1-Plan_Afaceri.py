652add9bc248d0bba2b184d0115b88d964ee722b
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-29 17:06:07 +0200

    Update 1-Plan_Afaceri.py

--- pages/1-Plan_Afaceri.py
+++ pages/1-Plan_Afaceri.py
@@ -3,7 +3,7 @@ import streamlit as st
 from docx import Document
 
 # Pagina 2 - Încărcare și completare document cu placeholder-uri
-st.write('Completare Document cu Placeholder-uri', '#7FBBE9')
+st.title(':blue[Completare Document cu Placeholder-uri]')
   
 # Încărcarea documentului cu placeholder-uri
 uploaded_template = st.file_uploader("Încărcați documentul cu placeholder-uri", type=["docx"], key="template")
