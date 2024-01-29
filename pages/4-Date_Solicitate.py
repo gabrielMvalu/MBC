b44ad3d048bb3fd83026ae524eafaf21d3526046
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 01:55:43 +0200

    Update 4-Date_Solicitate.py

--- pages/4-Date_Solicitate.py
+++ pages/4-Date_Solicitate.py
@@ -88,7 +88,7 @@ if uploaded_file is not None:
     st.session_state['date_solicitate'] = solicitate_data
 
     # Actualizează progresul
-    st.session_state.progress = max(st.session_state.progress, 25)  # completare si aactualizare progres bar // se completează 25%
+    st.session_state.progress += 25  
     st.sidebar.progress(st.session_state.progress)
     
 
