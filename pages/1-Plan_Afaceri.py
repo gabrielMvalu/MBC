ef33fb15325a342ddacfaf782afb3ee86e87f573
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-29 04:45:03 +0200

    Update 1-Plan_Afaceri.py

--- pages/1-Plan_Afaceri.py
+++ pages/1-Plan_Afaceri.py
@@ -23,12 +23,22 @@ if uploaded_template is not None:
             if "#Nr_recom" in run.text:
                 run.text = run.text.replace("#Nr_recom", st.session_state['date_generale']['Numărul de ordine în Registrul Comerțului'])
             
+   
+            # Înlocuirea dată înființare
             if "#Data_infiintare" in run.text:
                 run.text = run.text.replace("#Data_infiintare", st.session_state['date_generale']['Data înființării'])
             
+            # Condiționarea pentru adresa sediului
             if "#Adresa_sediu" in run.text:
-                adresa_sediu_text = f"Adresa sediu: {st.session_state['date_generale']['Adresa sediului social']}\n"
+                # Verifică dacă data înființării este egală cu "10.08.2012"
+                if st.session_state['date_generale']['Data înființării'] == "10.08.2012":
+                    adresa_sediu_text = "tralala"
+                else:
+                    adresa_sediu_text = f"Adresa sediu: {st.session_state['date_generale']['Adresa sediului social']}\n"
+                
+                # Înlocuiește placeholder-ul cu adresa de sediu condiționată
                 run.text = run.text.replace("#Adresa_sediu", adresa_sediu_text)
+
 
             
             if "#Adresa_pct_lucru" in run.text:
