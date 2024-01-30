2e425c620efb086be7cd777642f7c826c45583e1
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 22:08:58 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -10,43 +10,41 @@ if 'progress' not in st.session_state:
 st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
 
-def extrage_date_foaie(df):
-    valoare1 = "{:.2f}".format(df.iloc[9, 1])
-    valoare2 = "{:.2f}".format(df.iloc[10, 1])
-
+def extrage_date_bilant(df_bilant):
+    cpa20 = "{:.2f}".format(df.iloc[78, 1])
+    cpa21 = "{:.2f}".format(df.iloc[78, 2])
+    cpa22 = "{:.2f}".format(df.iloc[78, 3])
+    
     data = {
-        "Valoare1": valoare1, 
-        "Valoare2": valoare2,
+        "Capitalul propriu al actionarilor 2020": cpa20, 
+        "Capitalul propriu al actionarilor 2021": cpa21,
+        "Capitalul propriu al actionarilor 2022": cpa22
     }
-    return data
+    return data_bilant
 
 
 def incarca_si_extrage_date(uploaded_file):
     df_bilant = pd.read_excel(uploaded_file, sheet_name='1-Bilant')
     data_bilant = extrage_date_foaie(df_bilant)
+    
+    return data_bilant
 
-    df_contpp = pd.read_excel(uploaded_file, sheet_name='1-ContPP')
-    data_contpp = extrage_date_foaie(df_contpp)
-
-    return data_bilant, data_contpp
-
-st.header('Încărcare și Analiză Bilanț și Cont de Profit și Pierdere')
+st.header('Adaugati Analiză, Bilanț, Cont de Profit și Pierdere')
 
 uploaded_file = st.file_uploader("Adăugați fișierul aici sau faceți click pentru a încărca", type=["xlsx"])
 
 if uploaded_file is not None:
-    data_bilant, data_contpp = incarca_si_extrage_date(uploaded_file)
+    data_bilant = incarca_si_extrage_date(uploaded_file)
 
     # Stocarea datelor în st.session_state pentru utilizare ulterioară în aplicație
     st.session_state['data_bilant'] = data_bilant
-    st.session_state['data_contpp'] = data_contpp
 
     # Actualizează progresul
     st.session_state.progress += 25  
     st.sidebar.progress(st.session_state.progress)
 
-
-   
+    st.json(data_bilant)
+    
     st.write("Vizualizare Bilant:")
     st.dataframe(pd.DataFrame([data_bilant]))
     st.write("Vizualizare Analiza:")
