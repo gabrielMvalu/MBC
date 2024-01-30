c52fa8423577f3ca690f64b3e28799c6bf2609a8
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 22:49:40 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -8,35 +8,35 @@ if 'progress' not in st.session_state:
 st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
 
-def extrage_date_bilant(df_bilant):
-    cpa20 = df_bilant.iloc[1, 2]
-    cpa21 = df_bilant.iloc[78, 1]
-    cpa22 = df_bilant.iloc[79, 2]
+def extrage_date_bilant(df):
+    cpa20 = df.iloc[1, 2]
+    cpa21 = df.iloc[78, 1]
+    cpa22 = df.iloc[79, 2]
 
     data_bilant = {
         "Capitalul propriu al actionarilor 2020": cpa20, 
         "Capitalul propriu al actionarilor 2021": cpa21,
         "Capitalul propriu al actionarilor 2022": cpa22
     }
-    return data_bilant
+    return data
 
-def incarca_si_extrage_date(uploaded_file):
-    df_bilant = pd.read_excel(uploaded_file, sheet_name='1-Bilant')
-    data_bilant = extrage_date_bilant(df_bilant)
-    return data_bilant
-
-st.header('Adaugati Analiză, Bilanț, Cont de Profit și Pierdere')
+st.header(':blue[Adaugati Analiză, Bilanț, Cont de Profit și Pierdere]')
 
 uploaded_file = st.file_uploader("Adăugați fișierul aici sau faceți click pentru a încărca", type=["xlsx"])
 
 if uploaded_file is not None:
-    data_bilant = incarca_si_extrage_date(uploaded_file)
+    df = pd.read_excel(uploaded_file, sheet_name='1-Bilant')
+    data_bilant = extrage_data_bilant(df)
+
+    st.json({"Datele din bilant sunt:" data_bilant})
+    
+    st.write("Vizualizare Bilant:")
+    st.dataframe(pd.DataFrame([data_bilant]))
+    
     st.session_state['data_bilant'] = data_bilant
     st.session_state.progress += 25  
     st.sidebar.progress(st.session_state.progress)
 
     
-    st.json(data_bilant)
-    st.write("Vizualizare Bilant:")
-    st.dataframe(pd.DataFrame([data_bilant]))
+
 
