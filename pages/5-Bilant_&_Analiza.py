269045ec4a0251e38c862f726312bf9ac30d2e22
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 22:33:55 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -9,9 +9,9 @@ st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
 
 def extrage_date_bilant(df_bilant):
-    cpa20 = df_bilant.iloc[0, 1]
-    cpa21 = "{:.2f}".format(df_bilant.iloc[0, 2])
-    cpa22 = "{:.2f}".format(df_bilant.iloc[1, 2])
+    cpa20 = float(df_bilant.iloc[0, 1])
+    cpa21 = "{:.2f}".format(df_bilant.iloc[2, 78])
+    cpa22 = "{:.2f}".format(df_bilant.iloc[79, 2])
 
     data_bilant = {
         "Capitalul propriu al actionarilor 2020": cpa20, 
