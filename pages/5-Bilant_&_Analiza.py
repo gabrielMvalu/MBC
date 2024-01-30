bb319ddf1c5721f27e064a46a45f04bb0ba39e2e
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 22:12:45 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -2,31 +2,27 @@
 import streamlit as st
 import pandas as pd
 
-# Inițializarea progresului dacă nu există
 if 'progress' not in st.session_state:
     st.session_state.progress = 0
 
-# Afișează progress bar-ul în sidebar
 st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
 
 def extrage_date_bilant(df_bilant):
-    cpa20 = "{:.2f}".format(df.iloc[78, 1])
-    cpa21 = "{:.2f}".format(df.iloc[78, 2])
-    cpa22 = "{:.2f}".format(df.iloc[78, 3])
+    cpa20 = "{:.2f}".format(df_bilant.iloc[78, 1])
+    cpa21 = "{:.2f}".format(df_bilant.iloc[78, 2])
+    cpa22 = "{:.2f}".format(df_bilant.iloc[78, 3])
     
-    data = {
+    data_bilant = {
         "Capitalul propriu al actionarilor 2020": cpa20, 
         "Capitalul propriu al actionarilor 2021": cpa21,
         "Capitalul propriu al actionarilor 2022": cpa22
     }
     return data_bilant
 
-
 def incarca_si_extrage_date(uploaded_file):
     df_bilant = pd.read_excel(uploaded_file, sheet_name='1-Bilant')
-    data_bilant = extrage_date_foaie(df_bilant)
-    
+    data_bilant = extrage_date_bilant(df_bilant)
     return data_bilant
 
 st.header('Adaugati Analiză, Bilanț, Cont de Profit și Pierdere')
@@ -35,17 +31,12 @@ uploaded_file = st.file_uploader("Adăugați fișierul aici sau faceți click pe
 
 if uploaded_file is not None:
     data_bilant = incarca_si_extrage_date(uploaded_file)
-
-    # Stocarea datelor în st.session_state pentru utilizare ulterioară în aplicație
     st.session_state['data_bilant'] = data_bilant
-
-    # Actualizează progresul
     st.session_state.progress += 25  
     st.sidebar.progress(st.session_state.progress)
 
-    st.json(data_bilant)
     
+    st.json(data_bilant)
     st.write("Vizualizare Bilant:")
     st.dataframe(pd.DataFrame([data_bilant]))
-    st.write("Vizualizare Analiza:")
-    st.dataframe(pd.DataFrame([data_contpp]))
+
