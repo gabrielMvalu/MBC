906d119a7650ff5df9ebccd13907a35230f4a24f
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-30 23:01:31 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -9,23 +9,23 @@ st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
 
 def extrage_date_bilant(df):
-    cpa20 = df.iloc[1, 2]
-    cpa21 = df.iloc[78, 1]
-    cpa22 = df.iloc[79, 2]
+    cpa20 = df.iloc[78, 1]
+    cpa21 = df.iloc[78, 2]
+    cpa22 = df.iloc[79, 1]
 
     data_bilant = {
         "Capitalul propriu al actionarilor 2020": cpa20, 
         "Capitalul propriu al actionarilor 2021": cpa21,
         "Capitalul propriu al actionarilor 2022": cpa22
     }
-    return data_bilant
+    return data
 
 st.header(':blue[Adaugati Analiză, Bilanț, Cont de Profit și Pierdere]', divider='rainbow')
 
 uploaded_file = st.file_uploader("Adăugați fișierul aici sau faceți click pentru a încărca", type=["xlsx"])
 
 if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file, sheet_name='1-Bilant')
+    df = pd.read_excel(uploaded_file)
     data_bilant = extrage_date_bilant(df)
 
     st.json({"Datele din bilant sunt:": data_bilant})
