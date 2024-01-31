598de8e7d33d81656849f6c9e092a331833522c0
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-31 16:21:58 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -23,14 +23,13 @@ def extrage_date_bilant(df):
 def extrage_date_contpp(df1):
     ca20 = f"{df1.iloc[4, 1]:.2f}"
     ca21 = f"{df1.iloc[4, 2]:.2f}"
-    ca22 = df1.iloc[4, 3]
-    ca26 = df1.iloc[4, 6]
+    ca22 = f"{df1.iloc[4, 3]:.2f}"
     vt20 = f"{df1.iloc[55, 1]:.2f}"
     vt21 = f"{df1.iloc[55, 2]:.2f}"
     vt22 = f"{df1.iloc[55, 3]:.2f}"
 
     if ca26 != 0:
-        pc = f"{(ca26 / ca22 * 100) - 100:.2f}%"
+        pc = f"{(df1.iloc[4, 6] / df1.iloc[4, 3] * 100) - 100:.2f}%"
     else:
         pc = "N/A" 
     
