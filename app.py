2ee81680df1e76028c396de73714b9c2f634428d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-01 01:42:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,16 @@ if 'progress' not in st.session_state:
 # Afișează progress bar-ul în sidebar
 st.sidebar.write("Progresul tău:")
 st.sidebar.progress(st.session_state.progress)
+
+
+# Verifică dacă există informații despre utilizator
+if st.experimental_user:
+    user_email = st.experimental_user.email  # Obține adresa de email a utilizatorului
+    
+    # Verifică dacă adresa de email a utilizatorului corespunde
+    if user_email == 'marian@castemill.com':
+        # Afișează un buton dacă condiția este îndeplinită
+        if st.button('Buton Special pentru Marian'):
+            st.write('Bine ai venit, Marian!')
+else:
+    st.write("Informații despre utilizator nu sunt disponibile.")
