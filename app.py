70d6a1b6029ececa7d51e5260108d3248768b9d8
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-01 01:55:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # app.py
 import streamlit as st
 
-ADMIN_EMAIL = st.secrets["ADMIN_EMAIL"]
-
 
 st.header(':blue[Pagina Principală]', divider='rainbow')
 st.write(':violet[Bine ați venit la aplicația pentru completarea - Planului de Afaceri! -]')
@@ -17,9 +15,12 @@ st.sidebar.progress(st.session_state.progress)
 
 # Crează un câmp de input pentru adresa de email
 user_email = st.text_input('Introduceți adresa de email pentru acces special:')
-
-# Verifică dacă adresa de email a fost introdusă și corespunde
+# Verifică dacă adresa de email a fost introdusă
 if user_email:
+    # Accesează ADMIN_EMAIL aici, după ce adresa de email a fost introdusă
+    ADMIN_EMAIL = st.secrets["ADMIN_EMAIL"]
+
+    # Verifică dacă adresa de email introdusă corespunde cu ADMIN_EMAIL
     if user_email == ADMIN_EMAIL:
         # Afișează un buton dacă condiția este îndeplinită
         if st.button('Buton Special pentru Marian'):
