9d062670e59b6e82372cbc8b770b17dc3e6c9ec0
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-01 13:10:04 +0200

    Update 5-Bilant_&_Analiza.py

--- pages/5-Bilant_&_Analiza.py
+++ pages/5-Bilant_&_Analiza.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import altair as alt
 from altair import Scale, Color
 
-st.set_page_config(layout="wide", menu_items={'Get Help': 'https://www.castemill.com'})
+st.set_page_config(layout="wide")
 
 if 'progress' not in st.session_state:
     st.session_state.progress = 0
