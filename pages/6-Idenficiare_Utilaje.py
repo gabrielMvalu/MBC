e98467d6a83eaafe40b2be22f42d4f1e757012bd
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-02 11:48:04 +0200

    Update 6-Idenficiare_Utilaje.py

--- pages/6-Idenficiare_Utilaje.py
+++ pages/6-Idenficiare_Utilaje.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 
 # Creează o pagină nouă pe Streamlit
-st.header(':blue_book: Prelucrare XLSX', divider='rainbow')
+st.header(':blue[Prelucrare XLSX]', divider='rainbow')
 
 # Adaugă un widget de încărcare fișier pentru a permite utilizatorului să încarce un fișier XLSX
 uploaded_file = st.file_uploader("Încarcă un fișier XLSX", type=['xlsx'])
