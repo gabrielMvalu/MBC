455e641d615e51f3aaa557e193ee2e9dd19dd67a
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-03 17:54:05 +0200

    Update 1-Plan_Afaceri.py

--- pages/1-Plan_Afaceri.py
+++ pages/1-Plan_Afaceri.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from docx import Document
 from pages.constatator import extrage_informatii_firma, extract_asociati_admini, extract_situatie_angajati, extrage_coduri_caen
+from pages.datesolicitate import extrage_date_solicitate
 
 st.set_page_config(layout="wide")
 st.header(':blue[Completare Document cu Placeholder-uri]', divider='rainbow')
@@ -15,16 +16,25 @@ with col2:
     uploaded_document = st.file_uploader("Încărcați documentul Recom constatator.docx", type=["docx"], key="document")
     st.info('Se vor incarca doar documente docx si doar Raportul interogare', icon="ℹ️")
 
-if uploaded_template is not None and uploaded_document is not None:
+col3, col4 = st.columns(2)
+with col3:
+    uploaded_file1 = st.file_uploader("Încărcați fișierul aici sau faceți clic pentru a încărca", type=["xlsx"], key="document")
+with col4:
+    st.write('Text text')
+
+if uploaded_template is not None and uploaded_document is not None and uploaded_file1 is not None:
     template_doc = Document(uploaded_template)
     st.toast('Incepem procesarea Planului de afaceri', icon='⭐')     
     constatator_doc = Document(uploaded_document)
+    date_solicitate_doc = Document(uploaded_file1)
      
     informatii_firma = extrage_informatii_firma(constatator_doc)
     asociati_info, administratori_info = extract_asociati_admini(constatator_doc)
     situatie_angajati = extract_situatie_angajati(constatator_doc)
     coduri_caen = extrage_coduri_caen("\n".join([p.text for p in constatator_doc.paragraphs]))
-
+    date_solicitate = extrage_date_solicitate(date_solicitate_doc)
+    
+    
     def curata_duplicate_coduri_caen(coduri_caen):
         coduri_unice = {}
         for cod, descriere in coduri_caen:
@@ -49,6 +59,35 @@ if uploaded_template is not None and uploaded_document is not None:
         "#Administrator": administratori_text,
         "#activitatePrincipala": informatii_firma.get('Activitate principală','N/A'),
         "#CAENautorizate": coduri_caen_text,
+
+
+        "#categorie_intreprindere": date_solicitate.get('Categorie întreprindere', 'N/A'), 
+        "#Firme_legate": date_solicitate.get('Firme legate', 'N/A'),  
+        "#Tip_investitie": date_solicitate.get('Tipul investiției', 'N/A'),  
+        "#activitate": date_solicitate.get('Activitate', 'N/A'),
+        "#CAEN": date_solicitate.get('Cod CAEN', 'N/A'),
+        "#nr_locuri_munca_noi": date_solicitate.get('Număr locuri de muncă noi', 'N/A'),
+        "#Judet": date_solicitate.get('Județ', 'N/A'),
+        "#utilaj_dizabilitati": date_solicitate.get('Utilaj pentru persoane cu dizabilități', 'N/A'),
+        "#utilaj_cu_tocator": date_solicitate.get('Utilaj cu tocător', 'N/A'),
+        "#adresa_loc_implementare": date_solicitate.get('Adresa locației de implementare', 'N/A'),
+        "#nr_clasare_notificare": date_solicitate.get('Număr clasare notificare', 'N/A'),
+        "#clienti_actuali": date_solicitate.get('Clienți actuali', 'N/A'),
+        "#furnizori": date_solicitate.get('Furnizori', 'N/A'),
+        "#tip_activitate": date_solicitate.get('Tip activitate', 'N/A'),
+        "#ISO": date_solicitate.get('Certificări ISO', 'N/A'),
+        "#activitate_curenta": date_solicitate.get('Activitate curentă', 'N/A'),
+        "#dotari_activitate_curenta": date_solicitate.get('Dotări pentru activitatea curentă', 'N/A'),
+        "#info_ctr_implementare": date_solicitate.get('Informații despre contractul de implementare', 'N/A'),
+        "#zonele_vizate_prioritar": date_solicitate.get('Zonele vizate prioritare', 'N/A'),
+        "#utilaj_ghidare": date_solicitate.get('Utilaj de ghidare', 'N/A'),
+        "legaturi": date_solicitate.get('Legături', 'N/A'),
+        "#rude": date_solicitate.get('Rude în cadrul firmei', 'N/A'),
+        "#concluzie_CA": date_solicitate.get('Concluzie_CA', 'N/A'), 
+        "#caracteristici_tehnice": date_solicitate.get('Caracteristici tehnice relevante', 'N/A'),
+        "#flux_tehnologic": date_solicitate.get('Flux tehnologic', 'N/A'),
+        "#utilajeDNSH": date_solicitate.get('Utilaje DNSH', 'N/A'),
+        
     }
 
     def inlocuieste_in_tabele(tabele, placeholders):
