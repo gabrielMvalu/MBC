cd80265b45d1a2805d80ee19f05a98eb85746165
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-03 13:52:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ import streamlit as st
 st.header(':blue[Pagina Principală]', divider='rainbow')
 st.write(':violet[Bine ați venit la aplicația pentru completarea - Planului de Afaceri! -]')
 
-# Inițializarea progresului dacă nu există
-if 'progress' not in st.session_state:
-    st.session_state.progress = 0
-
-# Afișează progress bar-ul în sidebar
-st.sidebar.write("Progresul tău:")
-st.sidebar.progress(st.session_state.progress)
 
 
 user_email = st.text_input('Introduceți adresa de email pentru acces special:')
