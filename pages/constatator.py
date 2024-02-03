8c3608e47f6b4e5b3de9196d17450526fa869297
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-03 18:08:27 +0200

    Update constatator.py

--- pages/constatator.py
+++ pages/constatator.py
@@ -94,7 +94,7 @@ def extract_situatie_angajati(doc):
     }
     return data_angajati
 
-def extract_coduri_caen(doc_path):
+def extrage_coduri_caen(doc_path):
     doc = Document(doc_path)
     full_text = "\n".join(para.text.strip() for para in doc.paragraphs) 
     start_marker = "SEDII SI/SAU ACTIVITATI AUTORIZATE"
