49accbefa2670ef27e340bc70cb173b342cf6aba
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-03 03:39:15 +0200

    Update 2-Date_SRL.py

--- pages/2-Date_SRL.py
+++ pages/2-Date_SRL.py
@@ -1,8 +1,6 @@
-import streamlit as st
+# pages/2-Date_SRL.py
 from docx import Document
 import re
-import pandas as pd
-
 
 def extract_data_from_docx(doc):
     full_text = "\n".join(paragraph.text for paragraph in doc.paragraphs)
@@ -25,12 +23,10 @@ def extract_data_from_docx(doc):
     section_match = re.search(section_pattern, full_text)
     if section_match:
         section_text = section_match.group(1)
-        # Extragerea tuturor adreselor din secțiune
         secondary_address_pattern = re.compile(r"Adresă: (.*?)(?=\n)", re.DOTALL)
         adrese_secundare = re.findall(secondary_address_pattern, section_text)
     else:
         adrese_secundare = ["N/A"]
-    
     data = {
         "Denumirea firmei": firma,
         "Numărul de ordine în Registrul Comerțului": nr_ordine,
@@ -40,7 +36,6 @@ def extract_data_from_docx(doc):
         "Activitate principală": main_activity,
         "Adresa sediul secundar": adrese_secundare
     }
-
     return data
 
 def extract_detailed_info_from_docx(doc):
@@ -49,14 +44,12 @@ def extract_detailed_info_from_docx(doc):
     administratori = set()
     in_asociati_section = False
     in_persoane_imputernicite_section = False
-
     for i in range(len(text)):
         if "ASOCIAŢI PERSOANE FIZICE" in text[i]:
             in_asociati_section = True
             continue
         elif "REPREZENTANT acţionar/asociat/membru" in text[i]:
             in_asociati_section = False
-
         if in_asociati_section and "Calitate: " in text[i]:
             nume = text[i - 1].strip()
             j = i + 1
@@ -65,26 +58,21 @@ def extract_detailed_info_from_docx(doc):
             if j < len(text):
                 cota = text[j].split(":")[1].strip()
                 asociati[nume] = cota
-
         if "Persoane împuternicite (PERSOANE FIZICE)" in text[i]:
             in_persoane_imputernicite_section = True
             continue
         elif "Persoane împuternicite (PERSOANE JURIDICE)" in text[i]:
             in_persoane_imputernicite_section = False
-
         if in_persoane_imputernicite_section and "Calitate: " in text[i]:
             nume_admin = text[i - 1].strip()
             administratori.add(nume_admin)
-
     output_asociati = []
     for nume, cota in asociati.items():
         info = f"{nume} – asociat cu cota de participare la beneficii și pierderi {cota}"
         if nume in administratori:
             info += " și administrator"
         output_asociati.append(info)
-
-    nume_administrator = ', '.join(administratori)  # Gestionarea cazului cu mai mulți administratori
-
+    nume_administrator = ', '.join(administratori)  
     return output_asociati, nume_administrator
 
 def extract_situatie_angajati(doc):
@@ -105,13 +93,11 @@ def extract_situatie_angajati(doc):
     }
     return data_angajati
 
-
 def extract_caen_codes(full_text):
     start_marker = "SEDII SI/SAU ACTIVITATI AUTORIZATE"
     end_marker = "CONCORDAT PREVENTIV"
     caen_section_pattern = re.compile(rf"{start_marker}(.*?){end_marker}", re.DOTALL)
     caen_section_match = re.search(caen_section_pattern, full_text)
-    
     if caen_section_match:
         caen_section_text = caen_section_match.group(1)
         caen_code_pattern = re.compile(r"(\d{4}) - (.*?)\n")
@@ -119,51 +105,3 @@ def extract_caen_codes(full_text):
         return caen_codes
     else:
         return []
-
-# Încărcarea și procesarea documentului în Streamlit
-st.header(':blue[Încărcare Document Registrul Comerțului]',divider='rainbow')
-
-uploaded_file = st.file_uploader("Trageți fișierul aici sau faceți clic pentru a încărca un document", type=["docx"])
-
-if uploaded_file is not None:
-    doc = Document(uploaded_file)
-    general_data = extract_data_from_docx(doc)
-    detailed_info, admins = extract_detailed_info_from_docx(doc)
-    angajati_data = extract_situatie_angajati(doc)
-    caen_codes = extract_caen_codes("\n".join([p.text for p in doc.paragraphs]))
-
-
-
-    # Afișarea datelor în format JSON
-    st.json({
-        "Date Generale": general_data,
-        "Informații Detaliate": {"Asociați": detailed_info, "Administratori": admins},
-        "Situație Angajati": angajati_data,
-        "Coduri CAEN": caen_codes
-    })
-
-    # Afișarea datelor în DataFrame-uri pentru vizualizare
-    st.write("Date Generale:")
-    st.dataframe(pd.DataFrame([general_data]))
-
-    # Convertirea listei de administratori într-o listă de dicționare
-    admins_dict = [{"Administratori": admin} for admin in admins]
-    # Crearea DataFrame-ului din lista de dicționare
-    admins_df = pd.DataFrame(admins_dict)
-    # Afișarea DataFrame-ului în Streamlit
-    st.write("Administratori:")
-    st.dataframe(admins_df)
-
-
-    st.write("Situație Financiară:")
-    st.dataframe(pd.DataFrame([angajati_data]))
-
-    st.write("Coduri CAEN:")
-    st.dataframe(pd.DataFrame(caen_codes, columns=["Cod CAEN", "Descriere"]))
-
-    # După extragere, salvează datele în session_state
-    st.session_state['date_generale'] = general_data
-    st.session_state['date_detaliat'] = {"Asociați": detailed_info, "Administratori": admins}
-    st.session_state['situatie_angajati'] = angajati_data
-    st.session_state['coduri_caen'] = caen_codes
-
