cdb7ce1e999e610b58e408e6aae003f65059ccab
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-04 22:57:04 +0200

    Update ssiutilaje.py

--- pages/ssiutilaje.py
+++ pages/ssiutilaje.py
@@ -15,20 +15,34 @@ def extrage_pozitii(df_financiar):
 def coreleaza_date(date_financiar):
     df_amortizare = pd.read_excel('./assets/utilaje.xlsx', sheet_name='amortizare')
     df_utilaje = pd.read_excel('./assets/utilaje1.xlsx', sheet_name='utilajeservicii')
+    df_descriere = pd.read_excel(f'./assets/{st.session_state.codCAEN}.xlsx', sheet_name='utilajedescriere')
+    
     amortizare_data = {}
     servicii_data = {}
+    descriere_data = {}
+
     for index, row in df_amortizare.iterrows():
         if row.iloc[1]:
             nume = ' '.join(re.sub(r'\d+$', '', str(row.iloc[1])).strip().split()).lower()
             cod = ' '.join(str(row.iloc[2]).strip().split()) if row.iloc[2] else ''
             descriere = ' '.join(str(row.iloc[3]).strip().split()) if row.iloc[3] else ''
             amortizare_data[nume] = (cod, descriere)
+
     for index, row in df_utilaje.iterrows():
         if row.iloc[1]:
             nume = ' '.join(re.sub(r'\d+$', '', str(row.iloc[1])).strip().split()).lower()
             servicii_data[nume] = nume
+
+    for index, row in df_descriere.iterrows():
+        if row.iloc[1]:
+            nume = ' '.join(re.sub(r'\d+$', '', str(row.iloc[1])).strip().split()).lower()
+            descriere = ' '.join(str(row.iloc[2]).strip().split()) if row.iloc[2] else ''
+            descriere_data[nume] = descriere
+
     rezultate_corelate = []
     rezultate_corelate1 = []
+    rezultate_corelate2 = []
+
     for nume, cantitate in date_financiar:
         nume_curat = ' '.join(re.sub(r'\d+$', '', str(nume)).strip().split()).lower()
         if nume_curat in amortizare_data:
@@ -38,4 +52,9 @@ def coreleaza_date(date_financiar):
         if nume_curat in servicii_data:
             rezultat1 = f"{nume}, {cantitate} buc"
             rezultate_corelate1.append((nume, cantitate, rezultat1))
-    return rezultate_corelate, rezultate_corelate1
+        if nume_curat in descriere_data:
+            descriere = descriere_data[nume_curat]
+            rezultat2 = f"{nume} - {descriere}"
+            rezultate_corelate2.append((nume, cantitate, rezultat2))
+
+    return rezultate_corelate, rezultate_corelate1, rezultate_corelate2
