398c3f9ee053e78b75b1ef4554a35a7a92b82e43
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-07 22:04:08 +0200

    Update 7-Completare_Doc.py

--- pages/7-Completare_Doc.py
+++ pages/7-Completare_Doc.py
@@ -9,18 +9,28 @@ from pages.ssiutilaje import extrage_pozitii, coreleaza_date
 
 st.set_page_config(layout="wide")
 
-st.header(':blue[Procesul de inlocuire a Placeholder-uri]', divider='rainbow')
+st.header('Procesul de înlocuire a Placeholder-urilor', divider='rainbow')
 
-uploaded_template = st.file_uploader("Încărcați fisieru XLSX Date Solicitate", type=["xlsx"], key="dateSolicitate")
+col1, col2 = st.columns(2)
 
-if uploaded_template is not None:
-  datesolicitate_doc = pd.read_excel(uploaded_template)
-  date_din_xlsx_date_solicitate = extrage_date_solicitate(datesolicitate_doc)
-  
-  # Declararea si initializarea variabilei CAEN_extras cu codul CAEN extras
-  CAEN_extras = date_din_xlsx_date_solicitate['Cod CAEN']
+with col1:
+    uploaded_template = st.file_uploader("Încărcați fișierul XLSX Date Solicitate", type=["xlsx"], key="dateSolicitate")
 
-  # Afisarea mesajului cu numele firmei și codul CAEN
-  st.success(f"Vom începe prelucrarea firmei: {date_din_xlsx_date_solicitate['Denumirea firmei SRL']} cu prelucrarea pe codul CAEN: {date_din_xlsx_date_solicitate['Cod CAEN']}")
+    document_succes = False  # variabilă pentru a ține evidența succesului procesării primului document
 
+    if uploaded_template is not None:
+        datesolicitate_doc = pd.read_excel(uploaded_template)
+        date_din_xlsx_date_solicitate = extrage_date_solicitate(datesolicitate_doc)
 
+        CAEN_extras = date_din_xlsx_date_solicitate.get('Cod CAEN', 'Cod CAEN necunoscut')
+        firma = date_din_xlsx_date_solicitate.get('Denumirea firmei SRL', 'Firmă necunoscută')
+
+        st.success(f"Vom începe prelucrarea firmei: {firma} cu prelucrarea pe codul CAEN: {CAEN_extras}")
+
+        document_succes = True  # Setăm variabila pe True pentru a indica că primul document a fost procesat cu succes
+
+# Utilizarea celei de-a doua coloane pentru încărcarea celui de-al doilea document, dacă primul a fost procesat cu succes
+with col2:
+    if document_succes:
+        uploaded_file2 = st.file_uploader("Încărcați al doilea document", type=["pdf", "docx", "txt"], key="document2")
+        # Aici poți adăuga codul pentru procesarea celui de-al doilea document, după ce acesta este încărcat
