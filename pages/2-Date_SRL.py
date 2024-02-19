433a0a99f16fd085a43f8c6b15601afb99fb9a13
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-19 21:04:52 +0200

    Update 2-Date_SRL.py

--- pages/2-Date_SRL.py
+++ pages/2-Date_SRL.py
@@ -127,6 +127,7 @@ def extract_situatie_angajati(doc):
 
 
 def extrage_coduri_caen(full_text):
+    full_text = "\n".join(paragraph.text for paragraph in doc.paragraphs)
     start_marker = "SEDII SI/SAU ACTIVITATI AUTORIZATE"
     end_marker = "Denumire: Punct de lucru"
     pattern = fr"(?s){start_marker}(.*?){end_marker}"
